3bae669e90d6f673968d7daf3268e2de1c29b163
William Kimball <wkkimball043@gmail.com>
2018-09-22 15:23:40 -0500

    finished second guide

--- app.js
+++ app.js
@@ -1,17 +1,58 @@
-const app = new Vue({
-    el: '#app',
-    data: {
-        message: 'From Zero to Hero With Vue.js',
-        hasMessage: true,
-        vue:
-            'Vue.js is a thing that was created to serve a purpose, and that purpose is to make changing stuff on the DOM faster and easier to change. I hope this is correct, because I went off the template script in writing this.'
+const navbar = Vue.component('navbar', {
+    template: `
+        <header>
+            <ul>
+            <li>Home</li>
+            <li>About</li>
+            </ul>
+        </header>
+    `,
+});
+
+const counter = Vue.component('counter', {
+    template: `
+    <span>
+        <button @click="this.increment">+</button>
+        <span>{{sum}}</span>
+        <button @click="this.decrement">-</button>
+    </span>`,
+    data: () => {
+        return { sum: 0 };
     },
     methods: {
-        logMessage() {
-            console.log(this.message)
+        increment() {
+            this.sum++
         },
-        toggleMessage() {
-            this.hasMessage = !this.hasMessage
+        decrement() {
+            this.sum--
         }
+    }
+})
+
+const app = new Vue({
+  el: "#app",
+  data: {
+    message: "From Zero to Hero With Vue.js",
+    hasMessage: true,
+    vue:
+      "Vue.js is a thing that was created to serve a purpose, and that purpose is to make changing stuff on the DOM faster and easier to change. I hope this is correct, because I went off the template script in writing this.",
+    selected: "",
+    messages: [
+      { text: "Hello there!" },
+      { text: "Vue is cool!" },
+      { text: "Some more stuff that is very important!" }
+    ]
+  },
+  methods: {
+    logMessage() {
+      console.log(this.message);
     },
+    toggleMessage() {
+      this.hasMessage = !this.hasMessage;
+    }
+  },
+  components: {
+    navbar,
+    counter
+  }
 });
